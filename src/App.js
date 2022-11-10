ac2e91bbc21f00b06e2bf6190e86a9dd0fb304aa
Pankaj Vaishnav <97958495+pankaj-vaishnaw@users.noreply.github.com>
2022-11-10 14:43:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import Card from './Components/Card';
+import Img from './Images/img1.jpg';
+import Img2 from './Images/img3.jpg';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Card movie="IRONMAN" yearAndAuthor="2008,ANKURVERMA" images={Img} />
+      <Card
+        movie="HARRY POTTER"
+        yearAndAuthor="2000,ANKURVERMA"
+        images={Img2}
+      />
+      
+    </div>
+  );
+}
+
+export default App;
